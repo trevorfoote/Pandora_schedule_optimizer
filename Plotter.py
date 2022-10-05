85aebf92f7e1a12e0d2cb4bd2eeabbc9657e30e4
trevorfoote <trevor.o.foote@gmail.com>
2022-10-05 15:36:07 -0400

    Added functions to plot transit coverage Updated variable names for consistency

--- Plotter.py
+++ Plotter.py
@@ -21,16 +21,18 @@ plt.rcParams['font.size'] = '18'
 # Per Transit Coverage for GJ 3470b
 # Visibility calendar
 
-def Visibility_Calendar(input_dir, target_name, save_dir=None, save=False):
+
+
+def Visibility_Calendar(fdir, target_name, output_dir=None, save=False):
     """ Plot Visibility Calendar for target
         
     Parameters
     ----------
-    input_dir:      string
+    fdir:           string
                     directory containing input files 
     target_file:    string
                     name of csv file with list of targets
-    save_dir:     string
+    output_dir:       string
                     directory where to save output plots if save_fig = True
     save_fig:       bool
                     Argument whether to save a copy of the figure or not
@@ -41,7 +43,7 @@ def Visibility_Calendar(input_dir, target_name, save_dir=None, save=False):
         plot of transits schedule for year
     """
 ### Read in Visibility Data
-    data = pd.read_csv(input_dir + target_name + '/' + \
+    data = pd.read_csv(fdir + target_name + '/' + \
                        'Visibility for ' + target_name + '.csv', sep=',')
     t_mjd_utc = data['Time(MJD_UTC)']
     Earth_req = data['Earth_Clear']
@@ -85,7 +87,63 @@ def Visibility_Calendar(input_dir, target_name, save_dir=None, save=False):
     
 ### Save observing year plot
     if save == True:
-        plt.savefig(save_dir+'Observing Calendar for '
+        plt.savefig(output_dir+'Observing Calendar for '
+                +target_name+'.png')
+    plt.show()
+    plt.close()
+
+def Transit_Plots(fdir, target_name, output_dir=None, save=False):
+    data = pd.read_csv(fdir + target_name + '/' + \
+                       'Visibility for ' + target_name + '.csv', sep=',')
+    dt_iso_utc = Time(data['Time(MJD_UTC)'], format='mjd', scale='utc').to_value('datetime')
+    Visible = data['Visible']
+
+### Transit data
+    tdata = data[~data['All_Transits'].isnull()]
+    start_obs = Time(tdata['Observation_Start'], format='mjd', scale='utc').to_value('datetime')
+    end_obs = Time(tdata['Observation_Stop'], format='mjd', scale='utc').to_value('datetime')
+    start_transits = Time(tdata['Transit_Start'], format='mjd', scale='utc').to_value('datetime')
+    end_transits = Time(tdata['Transit_Stop'], format='mjd', scale='utc').to_value('datetime')
+
+    observation_coverage = tdata['Observation_Coverage']
+    transit_coverage = tdata['Transit_Coverage']
+
+    fig, axs = plt.subplots(1,1, figsize=(20,5))
+    axs.plot(dt_iso_utc, Visible * 0.5, 'b|')
+    axs.plot((start_obs, end_obs), (0.4, 0.4), '|', 
+            color='orange', ms=20, mew=3)
+    axs.plot((start_transits, end_transits), (0.4, 0.4), 'r-')
+
+    axs.set_title('Observing Window for %s' %target_name)
+    axs.xaxis.set_major_locator(mpl.dates.MonthLocator())
+    axs.xaxis.set_minor_locator(mpl.dates.DayLocator())
+    axs.xaxis.set_major_formatter(mpl.dates.DateFormatter('%Y-%b-%d'))
+    axs.set_yticks([0.4, 0.5])
+    axs.set_yticklabels(['Transit', 'Visible'])
+    for label in axs.get_xticklabels(which='major'):
+        label.set(rotation=30, horizontalalignment='right')
+    axs.set_ylim(0.35, 0.55)
+    axs.set_xlim(dt_iso_utc[0], dt_iso_utc[-1])
+
+### Save calendar of transits
+    if save == True:
+        plt.savefig(output_dir+'Transit Calendar for '
+                +target_name+'.png')
+    plt.show()
+    plt.close()
+
+
+### Plot Transit Coverage
+    plt.plot(transit_coverage*100, 'ro')
+    # plt.plot(observation_coverage*100, 'bo') #option for plotting observation coverage too
+    plt.ylim(0, 105)
+    plt.xlabel('Transit Number')
+    plt.ylabel('Coverage (%)')
+    plt.title('Per Transit Coverage for %s' %target_name)
+
+### Save calendar of transits
+    if save == True:
+        plt.savefig(output_dir+'Transit Coverage Plot for '
                 +target_name+'.png')
     plt.show()
     plt.close()
